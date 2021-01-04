20c2469c600ca6b37ce679d2aceb95fa1580413d
Janek <spikedevelopment@gmail.com>
2021-01-04 23:12:12 +0100

    added api

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Working!')
+// Unsplash API
+let count = 10;
+const apiKEY = ''
+const apiURL = `https://api.pexels.com/v1/search?query=nature&per_page=1`
+
+
+async function getPhotos() {
+    try {
+
+        const res = await fetch(apiURL, 
+           { 
+               headers: {
+                
+                    Authorization: apiKEY,
+            
+                },
+            }
+        );
+        const data = await res.json();
+
+        console.log('fetched photos', data);
+
+    } catch (error) {
+
+        console.log('Error fetching photos', error);
+
+    }
+}
+
+getPhotos()
