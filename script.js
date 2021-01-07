f02d952614cb2cdb8a76407d50a7e756bb1534db
Janek <spikedevelopment@gmail.com>
2021-01-07 11:39:23 +0100

    final

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 // Unsplash API
-let count = 10;
+let imageCount = 5;
 let currentPage = 1
-const apiURL = `https://api.pexels.com/v1/search?query=nature&page=${currentPage}&per_page=${count}`;
 
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
+
+function imageLoaded() {
+    imagesLoaded++
+
+    if(imagesLoaded === totalImages) {
+        ready = true
+        imagesLoaded = 0
+        loader.hidden = true
+        imageCount = 10
+    }
+
+}
 
 function setAttributes(element, attributes) {
     for (const key in attributes) {
-        element.setAttribute(key, attributes[key])
+        element.setAttribute(key, attributes[key]);
     }
 }
 
 function displayPhotos() {
+    totalImages = photosArray.length
+
     photosArray.forEach((photo) => {
         // Create link to image
         const item = document.createElement('a');
@@ -31,6 +48,9 @@ function displayPhotos() {
             title: photo.photographer
         })
 
+        // Event AudioListener, check when each finished loading
+        img.addEventListener('load', imageLoaded)
+
         // Create photo element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -38,6 +58,8 @@ function displayPhotos() {
 }
 
 async function getPhotos() {
+    
+    const apiURL = `https://api.pexels.com/v1/search?query=nature&page=${currentPage}&per_page=${imageCount}`;
 
     try {
 
@@ -53,15 +75,13 @@ async function getPhotos() {
 
         const data = await res.json();
         photosArray = await data.photos;
+        currentPage++
 
         displayPhotos()
 
-        console.log('fetched photos', data);
-        console.log('photos array', photosArray);
-
     } catch (error) {
 
-        console.log('Error fetching photos', error);
+        console.log('Error fetching photos');
 
     }
 
@@ -70,11 +90,9 @@ async function getPhotos() {
 // scroll loading of photos
 
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
-        // getPhotos();
-        let i = 0
-        i++
-        console.log('load more triggered', i);
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false
+        getPhotos();
     }
 })
 
